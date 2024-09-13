2020f20eef343588eb6031888a279ef05b717418
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-09-13 17:00:32 +0530

    Registe page is Completed

--- Frontend/contract/src/App.jsx
+++ Frontend/contract/src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Company_Profile from "./components/profile_forms/company_profile/Company_Profile";
 import Farmer_Profile from "./components/profile_forms/farmer_profile/Farmer_Profile";
+import Registeration from "./components/Registeration";
 
 function App() {
   return (
     <Router>
-      <div className="w-full min-h-screen" >
+      <div className="w-full min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/company_profile" element={<Company_Profile />} />
           <Route path="/farmer_profile" element={<Farmer_Profile />} />
+          <Route path="/register" element={<Registeration />} />
         </Routes>
       </div>
     </Router>
