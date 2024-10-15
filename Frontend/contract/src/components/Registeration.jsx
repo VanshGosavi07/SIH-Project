63d32f32242ed92cf60974417e413ef12eb1102f
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-10-15 09:03:30 +0530

    Login register password button enabled

--- Frontend/contract/src/components/Registeration.jsx
+++ Frontend/contract/src/components/Registeration.jsx
@@ -114,7 +114,7 @@ function Registeration() {
           <div className="col-span-2 mt-6 flex justify-center">
             <button
               type="submit"
-              className="px-8 py-2 bg-yellow-500 text-white rounded-full focus:outline-none hover:bg-yellow-600 z-10"
+              className="px-8 py-2 bg-yellow-500 text-white rounded-full focus:outline-none hover:bg-yellow-600 z-20"
             >
               Register
             </button>
