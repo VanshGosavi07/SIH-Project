c69642594af9cddf7ae3b58f1570546d7715ba32
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-10-15 08:48:54 +0530

    Password filed button is enabled for the login and register

--- Frontend/contract/src/components/Login.jsx
+++ Frontend/contract/src/components/Login.jsx
@@ -84,7 +84,7 @@ function Login() {
           <div className="mt-6 flex justify-center">
             <button
               type="submit"
-              className="px-8 py-2 bg-yellow-500 text-white rounded-full focus:outline-none hover:bg-yellow-600 z-10"
+              className="px-8 py-2 bg-yellow-500 text-white rounded-full focus:outline-none hover:bg-yellow-600 z-20"
             >
               Login
             </button>
