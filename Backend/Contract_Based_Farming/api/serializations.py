2c0a02e15ee79c40048c0181842443aa505b85cf
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-10-16 22:05:01 +0530

    Two Api is added of Company Profile and Farmer Profile

--- Backend/Contract_Based_Farming/api/serializations.py
+++ Backend/Contract_Based_Farming/api/serializations.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import Contract
+from .models import Contract, FarmerProfile, CompanyProfile
 
+
+# Create Contract
 class ContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
+        fields = '__all__'
+
+# Farmer Profile
+class FarmerProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FarmerProfile
+        fields = '__all__'
+
+
+# Company Profile
+class CompanyProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyProfile
         fields = '__all__'
